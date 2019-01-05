0a1942eb6955c7d9cb03916c1f4a8a470a3ccba5
thanmai <shaikraziyasultana@thanmai-iMac.local>
2019-01-05 15:22:11 +0530

    First Commit

---
+++ src/components/app/index.js
@@ -0,0 +1,127 @@
+import React from "react";
+
+import Table from "./table/index";
+import View from "./view/index";
+import Form from "./form/index";
+
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    Redirect,
+} from 'react-router-dom'
+
+
+
+
+
+const tableHeaders = ['Id','Name','Alias','Team'];
+
+
+
+class App extends React.Component{
+    state = {
+        tableValues : []
+    }
+
+    constructor(props) {
+        super(props)
+        this.createRecod = this.createRecod.bind(this)
+    }
+
+    fetchList() {
+        let self = this;
+        const request = new Request('/heroes', 
+        {method: 'GET', headers: {"Content-Type": "application/json"}});
+        fetch(request)
+        .then(res => res.json())
+        .then(function(data) {
+            self.setState({'tableValues': data});
+        });
+
+    }
+
+    componentDidMount() {
+        this.fetchList()
+
+    }
+
+    createRecod(name, alias, team) {
+        const self = this;
+        var body = {
+            name: name,
+            alias: alias,
+            team: team
+        }
+        var request = new Request('/heroes',{
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        fetch(request)
+        .then(function(){
+            self.fetchList()
+
+        });
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        //console.log(name, alias, team)
+        //const ID = (Math.random() * 100).toString()
+        //const newRecord = [ID, name, alias, team]
+        //const newTableValues = [...this.state.tableValues]
+        //newTableValues.push(newRecord)
+        //this.setState({tableValues: newTableValues})
+
+    }
+
+    
+
+
+        
+    
+    
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    <Route exact path="/list" render={(props) => {
+                        return <Table 
+                        values={this.state.tableValues} 
+                        headers={tableHeaders} 
+                        history={props.history}/>
+
+                    }}/>
+                    <Route exact path="/view/:id" component={View}/>
+                    <Route exact path="/create" render={(props) => {
+                        
+                        return <Form formSubmitCallback={this.createRecod} history={props.history}/>
+                        
+                    }}/>
+
+                    <Redirect to="/list" />
+                </Switch>
+              
+              
+             </Router>
+
+        );
+
+    }
+} 
+
+
+
+
+export default App;   
